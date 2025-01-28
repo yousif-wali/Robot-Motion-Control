750d9def81bdeeef744610a782ba3b927a3f668c
Yousif <wali.yousiff@gmail.com>
2025-01-28 10:55:08 +0300

    Added Light Sensor for the robot to follow

--- robot_motion_control.py
+++ robot_motion_control.py
@@ -14,22 +14,37 @@ rightMotor = robot.getDevice('right wheel motor')
 leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
 
+# Distance Sensors
 ds = []
 for i in range(8):
     ds.append(robot.getDevice('ps'+ str(i)))
     ds[-1].enable(TIME_STEP)
-
+# Light Sensors
+ls = []
+for i in range(8):
+    ls.append(robot.getDevice('ls' + str(i)))
+    ls[-1].enable(TIME_STEP)
+ 
 while robot.step(TIME_STEP) != -1:
     
-   sensor_values = []
+   distance_sensor_values = []
    for sensor in ds:
-       sensor_values.append(sensor.getValue())
-   sensor_values = np.asarray(sensor_values)
-   sensor_values = sensor_values / 400 * MAX_SPEED
-   print(sensor_values)
+       distance_sensor_values.append(sensor.getValue())
    
-   left_tire = MAX_SPEED - sensor_values[0] - sensor_values[1] - sensor_values[2]
-   right_tire = MAX_SPEED - sensor_values[7] - sensor_values[6] - sensor_values[5]
+   distance_sensor_values = np.asarray(distance_sensor_values)
+   distance_sensor_values = distance_sensor_values / 400 * MAX_SPEED
+   #print(distance_sensor_values)
+   
+   light_sensor_values = []
+   for sensor in ls:
+       light_sensor_values.append(sensor.getValue())
+   
+   light_sensor_values = np.asarray(light_sensor_values)
+   light_sensor_values = light_sensor_values / 31415 * MAX_SPEED
+   print(light_sensor_values)
+   
+   left_tire = MAX_SPEED - distance_sensor_values[0] - distance_sensor_values[1] - distance_sensor_values[2] + light_sensor_values[7] + light_sensor_values[6] + light_sensor_values[5] - light_sensor_values[3] - light_sensor_values[4]
+   right_tire = MAX_SPEED - distance_sensor_values[7] - distance_sensor_values[6] - distance_sensor_values[5] + light_sensor_values[0] + light_sensor_values[1] + light_sensor_values[2] - light_sensor_values[3] - light_sensor_values[4]
    
    leftMotor.setVelocity(left_tire)
    rightMotor.setVelocity(right_tire)
